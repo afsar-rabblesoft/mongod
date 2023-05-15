0e5cecd71a7085e43e236a68acd9f045c43f77e8
Pankaj Singh <pankaj.sandbox@gmail.com>
2023-05-15 15:50:42 +0530

    edit and delete

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const path = require("path");
 const express = require("express");
 const {mongoConnect} = require("./util/database");
 const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const bodyParser = require('body-parser');
 const app = express();
 
@@ -10,6 +12,7 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 mongoConnect(() => {
   // console.log(client);
