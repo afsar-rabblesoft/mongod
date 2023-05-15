032391244ab180fd8da7ea4ac825daa107233f19
Pankaj Singh <pankaj.sandbox@gmail.com>
2023-05-15 14:28:13 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const express = require("express");
+const {mongoConnect} = require("./util/database");
+const adminRoutes = require('./routes/admin');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/admin', adminRoutes);
+
+mongoConnect(() => {
+  // console.log(client);
+  app.listen(3000);
+});
